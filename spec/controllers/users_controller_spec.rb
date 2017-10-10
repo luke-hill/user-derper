7a274c4790b9398f4426916b70a27d6347c83d39
Louise Whittle <louise.whittle@hotmail.com>
2017-10-10 23:18:01 +0100

    created show page and testing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,17 +3,28 @@ require 'rails_helper'
 require_relative '../../app/controllers/users_controller'
 
 RSpec.describe UsersController, type: :controller do
+
   describe 'GET #index' do
     it 'returns a success response' do
       get :index, params: {}
-      
+
       expect(response).to be_success
     end
-    
+
     it 'assigns the @users instance variable' do
       get :index, params: {}
-      
+
       expect(assigns(:users)).to eq(User.all)
+    end
+  end
+
+  describe "GET #show" do
+    it "returns a success response" do
+      user = User.create!(first_name: "test",
+        surname: "test",
+        email: "test")
+      get :show, params: {id: user.to_param}
+      expect(response).to be_success
     end
   end
 end
