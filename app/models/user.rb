db71dddeb786bc93047b40c96fa5bee38ccd912f
Luke Hill <luke.hill@onthebeach.co.uk>
2017-10-11 16:18:32 +0100

    Cleaned up Model code for all users to be agnostic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   validates_presence_of :first_name, :surname, :email
+  validates :domain, format: { with: /\A(uk|se|no)\z/, message: 'only use valid domains' }
+  
+  def flag
+    "#{domain}_flag"
+  end
 end
