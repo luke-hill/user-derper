720e8dc8070ec4b68c76c09b55e21ce35d46bdd3
Luke Hill <luke.hill@onthebeach.co.uk>
2017-10-11 16:27:21 +0100

    Added new variables to re-split users on domain

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,17 @@
 class UsersController < ApplicationController
   before_action :find_user, only: %i[show edit update]
-  
+
   def index
     @users = User.all
+    @uk_users = uk_users
+    @se_users = se_users
+    @no_users = no_users
   end
 
   def show; end
-  
+
   def edit; end
-  
+
   def update
     if @user.update(user_params)
       redirect_to users_path
@@ -16,13 +19,25 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   private
+  
+  def uk_users
+    User.all.where(domain: 'uk')
+  end
+  
+  def se_users
+    User.all.where(domain: 'se')
+  end
+
+  def no_users
+    User.all.where(domain: 'no')
+  end
 
   def user_params
     params.require(:user).permit(:first_name, :surname, :email)
   end
-  
+
   def find_user
     @user = User.find(params[:id])
   end
