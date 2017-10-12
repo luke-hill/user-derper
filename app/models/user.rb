1c24bb20f0d6de3f65cf7fdec35d12bf306ed3c0
Louise Whittle <louise.whittle@hotmail.com>
2017-10-12 07:45:53 +0100

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
   has_many :holidays, primary_key: :id, foreign_key: :user_id
   has_many :searches, primary_key: :id, foreign_key: :user_id
-  
+
   validates_presence_of :first_name, :surname, :email
   validates :domain, format: { with: /\A(uk|se|no)\z/, message: 'is invalid' }
 
   def flag
     "#{domain}_flag"
   end
-  
+
   def last_search_info
     {
       when: last_search.searched_at,
@@ -16,17 +16,21 @@ class User < ApplicationRecord
       pax: formatted_pax
     }
   end
-  
+
+  def user_name
+    first_name.capitalize + ' ' + surname
+  end
+
   private
-  
+
   def location
     Destination.find(last_search.destination_id).name
   end
-  
+
   def formatted_pax
     "#{last_search.adults}/#{last_search.children}/#{last_search.infants}"
   end
-  
+
   def last_search
     @last_search ||= searches.last
   end
