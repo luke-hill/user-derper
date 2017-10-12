d7bd6a801e9c881a6b1911cf0fa8d8c0c7b08005
Louise Whittle <louise.whittle@hotmail.com>
2017-10-12 05:31:26 +0100

    hotel and desinataion linking

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,4 @@
 class Destination < ApplicationRecord
+  has_many :hotels, primary_key: :id, foreign_key: :destinations_id
   validates_presence_of :name
 end
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,3 +1,4 @@
 class Hotel < ApplicationRecord
+  belongs_to :destination, foreign_key: :destinations_id
   validates_presence_of :name, :destinations_id
 end
