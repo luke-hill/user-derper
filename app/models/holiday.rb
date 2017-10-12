97b559bb0ec6214cb972bd9edc0b5bde5b7e81dd
Luke Hill <luke.hill@onthebeach.co.uk>
2017-10-12 15:47:41 +0100

    stash

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,6 +1,6 @@
 class Holiday < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
-  #Last TBD
+  has_one :search, primary_key: :id, foreign_key: :search_id
   
   validates_presence_of :myb, :user_id, :search_id
 end
